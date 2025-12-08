8357eb756942c1bbcd761798c6a67a9f1a5d3c5b
Roman Strynzha <roman.strynzha@gmail.com>
2025-12-08 08:45:22 +0200

    add ErrorBoundary component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,46 @@
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
-import MainLayout from '../layouts/MainLayout';
-import Home from '../pages/Home/Home';
-import PostDetail from '../pages/Post/PostDetail'
-import SearchResults from '../pages/Search/SearchResults'
-import About from '../pages/About/About'
-import AuthorProfile from '../pages/Author/AuthorProfile'
-import AuthorPosts from '../pages/Author/AuthorPosts'
-import AuthorAbout from '../pages/Author/AuthorAbout'
-import CategoriesList from '../pages/Categories/CategoriesList'
-import CategoryDetail from '../pages/Categories/CategoryDetail'
-import AuthLayout from '../layouts/AuthLayout'
-import Login from '../pages/Auth/Login'
-import PostCreate from '../pages/Post/PostCreate'
-import PostEdit from '../pages/Post/PostEdit'
-import DashboardLayout from '../layouts/DashboardLayout'
-import DashboardHome from '../pages/Dashboard/DashboardHome'
-import MyPosts from '../pages/Dashboard/MyPosts'
-import Settings from '../pages/Dashboard/Settings'
-import NotFound from '../pages/NotFound/NotFound'
+import ErrorBoundary from '../pages/ErrorBoundary/ErrorBoundary';
 
+// Layouts
+import MainLayout from '../layouts/MainLayout';
+import AuthLayout from '../layouts/AuthLayout';
+import DashboardLayout from '../layouts/DashboardLayout';
+
+// Public pages
+import Home from '../pages/Home/Home';
+import PostDetail from '../pages/Post/PostDetail';
+import SearchResults from '../pages/Search/SearchResults';
+import About from '../pages/About/About';
+
+// Authors
+import AuthorProfile from '../pages/Author/AuthorProfile';
+import AuthorPosts from '../pages/Author/AuthorPosts';
+import AuthorAbout from '../pages/Author/AuthorAbout';
+
+// Categories
+import CategoriesList from '../pages/Categories/CategoriesList';
+import CategoryDetail from '../pages/Categories/CategoryDetail';
+
+// Auth
+import Login from '../pages/Auth/Login';
+
+// Posts
+import PostCreate from '../pages/Post/PostCreate';
+import PostEdit from '../pages/Post/PostEdit';
+
+// Dashboard
+import DashboardHome from '../pages/Dashboard/DashboardHome';
+import MyPosts from '../pages/Dashboard/MyPosts';
+import Settings from '../pages/Dashboard/Settings';
+
+// Not found
+import NotFound from '../pages/NotFound/NotFound';
 
 const AppRouter = () => {
 	return (
-		<Routes>
+		<Routes errorElement={<ErrorBoundary />}>
+			{/* Public routes */}
 			<Route path="/" element={<MainLayout />}>
 				<Route index element={<Home />} />
 				<Route path="posts/:id" element={<PostDetail />} />
@@ -44,36 +61,45 @@ const AppRouter = () => {
 				</Route>
 			</Route>
 
-			{/* Login */}
+			{/* Auth */}
 			<Route path="/login" element={<AuthLayout />}>
 				<Route index element={<Login />} />
 			</Route>
 
-			{/* Posts create/edit */}
-			<Route path="/posts/new" element={
-				<ProtectedRoute isAuthenticated={true}>
-					<PostCreate />
-				</ProtectedRoute>
-			} />
+			{/* Create & Edit Posts */}
+			<Route
+				path="/posts/new"
+				element={
+					<ProtectedRoute isAuthenticated={true}>
+						<PostCreate />
+					</ProtectedRoute>
+				}
+			/>
 
-			<Route path="/posts/:id/edit" element={
-				<ProtectedRoute isAuthenticated={true}>
-					<PostEdit />
-				</ProtectedRoute>
-			} />
+			<Route
+				path="/posts/:id/edit"
+				element={
+					<ProtectedRoute isAuthenticated={true}>
+						<PostEdit />
+					</ProtectedRoute>
+				}
+			/>
 
 			{/* Dashboard */}
-			<Route path="/dashboard" element={
-				<ProtectedRoute isAuthenticated={true}>
-					<DashboardLayout />
-				</ProtectedRoute>
-			}>
+			<Route
+				path="/dashboard"
+				element={
+					<ProtectedRoute isAuthenticated={true}>
+						<DashboardLayout />
+					</ProtectedRoute>
+				}
+			>
 				<Route index element={<DashboardHome />} />
 				<Route path="my-posts" element={<MyPosts />} />
 				<Route path="settings" element={<Settings />} />
 			</Route>
 
-			{/* Not Found */}
+			{/* Fallback */}
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
