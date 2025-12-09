bf168327b0c78a7e04ae30b8821fba3c79521df4
Roman Strynzha <roman.strynzha@gmail.com>
2025-12-09 10:58:11 +0200

    add NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import cls from './NotFound.module.css';
 
 const NotFound = () => {
+	const navigate = useNavigate();
 	return (
-		<div>
-			NotFound
-		</div>
-	)
-}
+		<div className={cls.notFound}>
+			<h1 className={cls.headerText}>404</h1>
+			<p className={cls.text}>Сторінку не знайдена</p>
 
-export default NotFound
+			<button
+				type="button"
+				className={`${cls.buton} ${cls.buttonHome}`}
+				onClick={() => navigate('/')}
+			>
+				На головну
+			</button>
+			<button
+				type="button"
+				onClick={() => navigate(-1)}
+				className={`${cls.buton} ${cls.buttonBack}`}
+			>
+				Назад
+			</button>
+		</div>
+	);
+};
+
+export default NotFound;
