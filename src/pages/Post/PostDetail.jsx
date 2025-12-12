b45a9c6e92501191c6d80fb998c17c414b0ab908
Roman Strynzha <roman.strynzha@gmail.com>
2025-12-12 12:47:35 +0200

    add PostDetails

--- src/pages/Post/PostDetail.jsx
+++ src/pages/Post/PostDetail.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { posts } from '../../data/mockPosts';
+import { authors } from '../../data/mockAuthors';
+import { categories } from '../../data/mockCategories';
+import cls from './PostDetail.module.css';
 
 const PostDetail = () => {
-	return (
-		<div>
-			PostDetail
-		</div>
-	)
-}
+    const { id } = useParams();
+    
+    // Порівнюємо число з числом
+    const postId = Number(id);
+    const post = posts.find(post => post.id === postId);
 
-export default PostDetail
+    if (!post) {
+        return <h2>Пост не знайдено</h2>;
+    }
+
+    const author = authors.find(author => author.id === post.authorId);
+    const category = categories.find(category => category.id === post.categoryId);
+
+    const dateCreatedAt = new Date(post.createdAt).toLocaleString('uk-UA');
+
+    return (
+        <div className={cls.post}>
+            <h3>{post.title}</h3>
+
+            <div className={cls.meta}>
+                {author && (
+                    <span>
+                        Автор:{' '}
+                        <Link to={`/authors/${post.authorId}`}>
+                            {author.name}
+                        </Link>
+                    </span>
+                )}
+
+                {category && (
+                    <span>
+                        Категорія:{' '}
+                        <Link to={`/categories/${post.categoryId}`}>
+                            {category.name}
+                        </Link>
+                    </span>
+                )}
+
+                <span className={cls.cardDate}>{dateCreatedAt}</span>
+                <span className={cls.cardViews}>👁 {post.views}</span>
+            </div>
+
+            {post.imageUrl && (
+                <div className={cls.image}>
+                    <img src={post.imageUrl} alt={post.title} />
+                </div>
+            )}
+
+            <p className={cls.text}>{post.content}</p>
+
+            <div className={cls.tags}>
+                {post.tags?.map(tag => (
+                    <Link key={tag} to={`#${tag}`}>
+                        #{tag}
+                    </Link>
+                ))}
+            </div>
+
+            {/* Тут можна буде додати кнопку "Редагувати" */}
+        </div>
+    );
+};
+
+export default PostDetail;
